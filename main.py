2696ee4f9b10b65501d02b8e0d6c73c73d387b2b
JoanCurtoAlonso <joan.curto01@estudiant.upf.edu>
2017-03-05 12:26:42 +0100

    empezando

---
+++ main.py
@@ -0,0 +1,19 @@
+import sys
+import string
+
+
+def read():
+
+
+def main():
+
+    corpus = open("corpus.txt","r")
+    lexic = open("lexic.txt", "w")
+    for line in corpus:
+        words = line.split("    ")
+        
+
+
+
+if __name__ == "__main__":
+    main()

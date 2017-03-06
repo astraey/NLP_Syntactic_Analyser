6089e9c345a2fe6da50f873d1dee5d1df704220d
JoanCurtoAlonso <joan.curto01@estudiant.upf.edu>
2017-03-06 11:57:36 +0100

    lexic creado

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
 import sys
 import string
+from collections import namedtuple
 
-
-def read():
 
 
 def main():
 
     corpus = open("corpus.txt","r")
+    lexid = {}
     lexic = open("lexic.txt", "w")
-    for line in corpus:
-        words = line.split("    ")
-        
 
+    for linec in corpus:
+        linec = linec.decode("latin_1").encode("UTF-8")
+
+        linec = linec.replace("\r\n","")
+        linec = linec.replace('\n', '')
+        wordsc = linec.split("\t")
+        #print wordsc
+
+        if wordsc[0] in lexid:
+            if wordsc[1] in lexid[wordsc[0]]:
+                lexid[wordsc[0]][wordsc[1]] += 1
+            else:
+                lexid[wordsc[0]] = {wordsc[1]: 1}
+        else:
+            lexid[wordsc[0]] = {wordsc[1]: 1}
+
+    #print lexid
+    lexid
+    for key1 in lexid:
+        for key2 in lexid[key1]:
+            lexic.write(key1 + "\t" + key2 + "\t" + str(lexid[key1][key2]) + "\n")
 
 
 if __name__ == "__main__":
